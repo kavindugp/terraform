60cac5f4d8386bd523b20c0af368aa674c02a3d8
Kavindu Geesara <38973598+kavindugp@users.noreply.github.com>
2021-06-27 22:04:33 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,4 +3,6 @@ variable "path" {default = "/root/terraform/access-key"}
 provider "google" {
     project = "neon-infinity-233017"
     region = "us-east1-b"
-    credentials = "${file("${var.path}/secrets.json")}"
+    credintials = "${file("${var.path}/secrets.json")}"
+
+}
